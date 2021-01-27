b72f03e0aa583c010f0014f53f974b29cd328621
jtekippe <jtekippe@gmail.com>
2021-01-27 16:25:07 -0500

    Update alien_invasion.py
    Set the background color and added a line to clear the screen during each loop

--- VideoGame/alien_invasion.py
+++ VideoGame/alien_invasion.py
@@ -11,6 +11,8 @@ class AlienInvasion:
 
 		self.sreen = pygame.display.set_mode((1200, 800))
 		pygame.display.set_caption("Alien Invasion")
+		#set bg color
+		self.bg_color = (230, 230, 230)
 
 	def run_game(self):
 		"""Start the main loop of the game"""
@@ -19,6 +21,8 @@ class AlienInvasion:
 			for event in pygame.event.get():
 					if event.type == pygame.QUIT:
 						sys.exit()
+			#redraw the screen on each pass through the loop
+			self.screen.fill(self.bg_color)
 			#render most recently drawn screen
 			pygame.display.flip()
 
@@ -26,4 +30,3 @@ if __name__ == '__main__':
 	#make a game instance and run the game
 	ai = AlienInvasion()
 	ai.run_game()
-	
