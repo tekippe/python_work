3edb45ebe77d2b2955d0c4112a0d79425031effc
jtekippe <jtekippe@gmail.com>
2021-02-01 12:12:55 -0500

    refactored event listeners

--- VideoGame/alien_invasion.py
+++ VideoGame/alien_invasion.py
@@ -35,16 +35,27 @@ class AlienInvasion:
 			if event.type == pygame.QUIT:
 				sys.exit()
 			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RIGHT:
+				self._check_keydown_events(event)
+			elif event.type == pygame.KEYUP:
+				self._check_keyup_events(event)
+			
+			
+
+	def _check_keydown_events(self, event):
+		if event.type == pygame.KEYDOWN:	
+			if event.key == pygame.K_RIGHT:
 					#move ship to the right
 					self.ship.moving_right = True
-				elif event.key == pygame.K_LEFT:
+			elif event.key == pygame.K_LEFT:
 					self.ship.moving_left = True
-			elif event.type == pygame.KEYUP:
+
+	def _check_keyup_events(self, event):
+		if event.type == pygame.KEYUP:
 				if event.key == pygame.K_RIGHT:
 					self.ship.moving_right = False
 				elif event.key == pygame.K_LEFT:
 					self.ship.moving_left = False
+
 		
 					
 
